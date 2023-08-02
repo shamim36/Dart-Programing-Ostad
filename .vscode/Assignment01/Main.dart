45e22969e2af0f7dc44e34d991e15152e86680d8
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-08-02 21:27:37 +0600

    Update Main.dart

--- .vscode/Assignment01/Main.dart
+++ .vscode/Assignment01/Main.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('Hello World');
+  const int a = 7;
+  const int b = 3;
+
+  int add = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  double div = a / b;
+  int mod = a % b;
+
+  print('Addition: $add');
+  print('Subtraction: $sub');
+  print('Multiplication: $mul');
+  print('Division: $div');
+  print('Modulus: $mod');
 }
