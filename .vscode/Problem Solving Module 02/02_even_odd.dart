41257f42fa16394ece86b77b484ba4e5e9f6f4ed
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-08-08 11:23:23 +0600

    Update 02_even_odd.dart

--- .vscode/Problem Solving Module 02/02_even_odd.dart
+++ .vscode/Problem Solving Module 02/02_even_odd.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 void main() {
   int num = int.parse(stdin.readLineSync()!);
   if (num % 2 == 0) {
-    print('even');
+    print('$num is an even number.');
+  } else {
+    print('$num is an odd number.');
   }
 }
