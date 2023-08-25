d309026b624309c34a0090c9ba891614cd618f0d
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-08-25 23:40:16 +0600

    Create Main.dart

---
+++ .vscode/Live Quiz Module 04/Main.dart
@@ -0,0 +1,24 @@
+base class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
+
+base class Song extends Media {
+  String artist;
+
+  Song({required this.artist});
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+
+void main() {
+  Media media = new Media();
+  Song song = new Song(artist: 'Jennie');
+
+  media.play();
+  song.play();
+}
